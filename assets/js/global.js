e5f11e40d99b525077795b43a6c21a5f148230c0
Lucky Goswami <lgoswami204@gmail.com>
2024-03-05 15:59:25 +0530

    feat: search toggling for small screens

---
+++ assets/js/global.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// add event on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) {
+    elem.addEventListener(eventType, callback);
+  }
+};
+
+// toggle search box in small screen
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", () => {
+  searchBox.classList.toggle("active");
+});
